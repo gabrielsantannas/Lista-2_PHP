e4dad3f19dc7821b47c81ef62e95f8ddf2e65475
Gabriel SantAnna <gsantanna00517@gmail.com>
2025-06-27 23:22:20 -0300

    atividade10

---
+++ Atividade10/atividade10.php
@@ -0,0 +1,9 @@
+<?php
+
+$comprimento = $_POST['comprimento'];
+$largura = $_POST['largura'];
+$altura = $_POST['altura'];
+
+$volume = $comprimento * $largura * $altura;
+
+echo "O volume da caixa é: " . $volume . " metros cúbicos.<br>";
